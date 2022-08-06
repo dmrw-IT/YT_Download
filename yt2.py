1c824f076cbc0063bd72ea47b62a96fe61cd59cb
dmrw-IT <dmrw-IT@protonmail.com>
2022-08-06 13:36:09 -0300

    Added: Refactor and General Formatting Clean-up.

--- yt2.py
+++ yt2.py
@@ -4,7 +4,8 @@ import os, os.path, sys
 from pytube import YouTube
 from pytube import Playlist
 
-######
+##### TO DO: #####
+
 ### : Implement Playlists
 # 2 : Implement Progress Bars
 # 3 : Implement "Get Latest Video from a Channel"
@@ -18,98 +19,93 @@ from pytube import Playlist
 # V_Test_URL 2: https://www.youtube.com/watch?v=ssvdczn-9qQ
 # P_Test_URL: https://www.youtube.com/playlist?list=PLlQRht9pdQyQBqKt-5wuuKoZ7ohJ5A1Y5
 
-# Functions
+##### Functions #####
 
-# CLEAN_STRING() : Strip certain characters out of Title used for Filename:
-### Future Implementation: REGEX ###
+# Strip certain characters out of Title used for Filename:
 def clean_string(title_string):
     elim_dict = {"|":"", " ":"_", "'":"" }
     table = title_string.maketrans(elim_dict)
     title_string = title_string.translate(table)
-    print (title_string)
-
+    
+# Function to handle downloading single YT Video:
 def video():
-    # Prompt User to provide a YouTube Video link:
-    url = input('Enter the YouTube Video link you would like to download: ')
-    print("Downloading:")
+    # Prompt User to provide a YT Video link:
+    url = input('Enter the YT Video link you would like to download: ')
+    
     # Set Variable for YouTube object:
     yt = YouTube(url)
+
     # Set Output File path for current Linux user:
-    ### Future Implementation: Incorporate ALL operating systems ###
     yt_dir = "/YT_Downloads/videos/"
     full_path = os.path.expanduser('~') + yt_dir
-
     if not os.path.exists(full_path):
         os.mkdir(full_path)
 
-    # Get the Title of the video and replace the spaces with underscores:
+    # Get the Title of the Video:
     title = yt.title
-
     clean_string(title)
 
-    # Target a Stream that is Progressive and has 720p resolution:
+    # Target Stream by itag(22). This itag generally Progressive and 720p resolution:
+    # See PyTube documentation for more information about Streams and StreamQueries #
     stream = yt.streams.get_by_itag(22)
 
     # Download the video:
+    print(f"Downloading: {yt.title}")
     stream.download(output_path=full_path, filename=title)
-    #Confirm DL and Restart the Program:
+    
     print("\nDownload Complete: Restarting the Program...\n")
     
-
 def playlist():
-    # Prompt User to provide a YouTube Video link:
-    url = input('Enter the YouTube Playlist link you would like to download: ')
-    # Set Variable for YouTube object:
-    p = Playlist(url)
-    # Set Output File path for current Linux user:
-    ### Future Implementation: Incorporate ALL operating systems ###
 
+    # Prompt User to provide a YT Playlist link:
+    url = input('Enter the YT Playlist link you would like to download: ')
+
+    # Set Variable for Playlist object:
+    p = Playlist(url)
+
+    # Create proper directory paths if they don't already exist:
     p_dir = "/YT_Downloads/playlists/"
     full_path = os.path.expanduser('~') + p_dir
-
     if not os.path.exists(full_path):
         os.mkdir(full_path)
 
-    # Get the Title of the video and replace the spaces with underscores:
+    # Get the Title of the Playlist:
     title = p.title
     clean_string(title)
 
-    # Target a Stream that is Progressive and has 720p resolution:
-    
-    #print(f"Downloading Playlist: {p.title} \n")
-
+    # Handling Single Video title output for video/p.video for loop:
     video_links = p.video_urls
     video_titles = []
     vl_count = 0
-
     for link in video_links:
         video_titles.append(YouTube(link).title)
 
+    print(f"Downloading Playlist: {title}\n")
+
+    # Loop that handles Playlist Download:
     for video in p.videos:
-        print(f"Downloading:\n{video_titles[vl_count]}")
+        print(f"Downloading: {video_titles[vl_count]}")
         video.streams.get_by_itag(22).download(output_path=full_path)
         vl_count += 1
 
-    #Confirm DL and Restart the Program:
     print("\nDownload Complete: Restarting the Program...\n")
                 
 def exit_program(counter, main):
-    
+    # Handle Exit logic: Counter reaches 5, Exit Program.
     if counter == 5:
         print(f"{counter}/5 attempts remaining...")
         main = False
         print("Exiting Program: Too Many Errors. Thank you for using YT_Download.")
         sys.exit()
+    # main set to False, Exit Program:
     elif main == False:
         print("Thank you for using YT_Download")
         sys.exit()
     
-
 class my_exception(Exception):
     pass
 
-######## Main #########
-
+##### Main #####
 user_choice = [1,2,3]
 counter = 0
 main = True
@@ -128,7 +124,6 @@ while main == True:
         elif v_or_p in user_choice and v_or_p == 2:
             playlist()
             
-
         elif v_or_p in user_choice and v_or_p == 3:
             main = False
 
@@ -140,7 +135,6 @@ while main == True:
         counter += 1
         exit_program(counter)
         
-    
     except:
         print("Make sure to enter a 1 for Video or a 2 for Playlist or 3 to exit the program.")
         counter += 1
